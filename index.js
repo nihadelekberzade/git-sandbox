9a7afab23fb23c0ffcd1b75fa612d691f770ee82
nihad <elekberzadeh333@gmail.com>
2021-07-23 01:38:30 +0400

    get-name branch added with functioanlity

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const user = {
   name: 'jackson',
   age: 23,
 }
-const getId = (user) => {
-  return user.id
-}
+
 const user2 = {
   id: 4,
   name: 'paul',
   age: 20,
+}
+const getId = (user) => {
+  return user.id
+}
+const getName = (user) => {
+  return user.name
 }
